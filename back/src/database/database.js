d1b8540469f5f234d421473c1748005905ffb346
erick <erick.sanchezcorrea@gmail.com>
2023-06-26 01:49:54 -0500

    se modificó el variable db_port

--- back/src/database/database.js
+++ back/src/database/database.js
@@ -2,11 +2,7 @@ import Sequelize from 'sequelize'
 import pg from 'pg'
 import {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER} from '../config.js'
 
-// export const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-//     host: DB_HOST,
-//     dialect: 'postgres' 
-// });
 
-export const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_DATABASE}`, {
-    dialectModule: pg
-  })
+export const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+  dialectModule: pg
+})
