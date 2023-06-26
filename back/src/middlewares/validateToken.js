685e614d4204110f34dc8acc2dd125633c4da7eb
erick <erick.sanchezcorrea@gmail.com>
2023-06-25 23:10:22 -0500

    Revert "se modificó FIREBASE_CREDENTIAL_private_key"
    This reverts commit 0a0c5f57ae27a9034669c47704a6b3775d6b8e07.

--- back/src/middlewares/validateToken.js
+++ back/src/middlewares/validateToken.js
@@ -2,16 +2,12 @@ import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import admin from 'firebase-admin'
 import { FIREBASE_CREDENTIAL_type, FIREBASE_CREDENTIAL_project_id, FIREBASE_CREDENTIAL_private_key_id, FIREBASE_CREDENTIAL_private_key,  FIREBASE_CREDENTIAL_client_email, FIREBASE_CREDENTIAL_client_id, FIREBASE_CREDENTIAL_auth_uri, FIREBASE_CREDENTIAL_token_uri, FIREBASE_CREDENTIAL_auth_provider_x509_cert_url, FIREBASE_CREDENTIAL_client_x509_cert_url, FIREBASE_CREDENTIAL_universe_domain } from "../config.js";
 
-const { privateKey  }  =  JSON.parse(FIREBASE_CREDENTIAL_private_key)
-// private_key_id: FIREBASE_CREDENTIAL_private_key_id,
-
-
 initializeApp({
     // credential: applicationDefault(),
     credential: cert({
         type: FIREBASE_CREDENTIAL_type,
         project_id: FIREBASE_CREDENTIAL_project_id,
-        privateKey,
+        private_key_id: FIREBASE_CREDENTIAL_private_key_id,
         private_key: FIREBASE_CREDENTIAL_private_key,
         client_email: FIREBASE_CREDENTIAL_client_email,
         client_id: FIREBASE_CREDENTIAL_client_id,
