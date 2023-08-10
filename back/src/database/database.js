f7ebdba6f917926c289851a65a9287a714cdab91
erick <erick.sanchezcorrea@gmail.com>
2023-08-10 17:45:36 -0500

    cambio de base de datos

--- back/src/database/database.js
+++ back/src/database/database.js
@@ -4,5 +4,8 @@ import {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER} from '../config.js'
 
 
 export const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
-  dialectModule: pg
-})
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: true, // Habilitar SSL
+  },
+});
